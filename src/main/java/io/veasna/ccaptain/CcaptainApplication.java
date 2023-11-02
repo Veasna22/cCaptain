32c36e85a90f0cf85eea8e26af4032efdca50e9b
Ea Veasna <eaveasna52@gmail.com>
2023-11-02 14:10:54 +0700

    init project

---
+++ src/main/java/io/veasna/ccaptain/CcaptainApplication.java
@@ -0,0 +1,13 @@
+package io.veasna.ccaptain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcaptainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcaptainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/veasna/ccaptain/CcaptainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.veasna.ccaptain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcaptainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
