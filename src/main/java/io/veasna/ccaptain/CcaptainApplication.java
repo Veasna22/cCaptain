d3f52d11b6481a603c0012da2e32fe3185322ece
Ea Veasna <eaveasna52@gmail.com>
2023-11-04 21:14:42 +0700

    chore: exclude SecurityAutoConfig + Bean BCrypt

--- src/main/java/io/veasna/ccaptain/CcaptainApplication.java
+++ src/main/java/io/veasna/ccaptain/CcaptainApplication.java
@@ -2,12 +2,20 @@ package io.veasna.ccaptain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CcaptainApplication {
+	private static final int STRENGHT = 12;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CcaptainApplication.class, args);
 	}
-
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder(STRENGHT);
+	}
 }
