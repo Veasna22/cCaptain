0e0998cc6eb71ef38e52711499ead4e049b199d7
Ea Veasna <eaveasna52@gmail.com>
2023-11-27 14:47:57 +0700

    fix(LoginForm) : add validation and annotation

--- src/main/java/io/veasna/ccaptain/form/LoginForm.java
+++ src/main/java/io/veasna/ccaptain/form/LoginForm.java
@@ -1,7 +1,10 @@
 package io.veasna.ccaptain.form;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Veasna
@@ -9,11 +12,13 @@ import lombok.Data;
  * @license Veasna , LLC
  * @since 4/11/23 22:17
  */
-@Data
+@Getter
+@Setter
 public class LoginForm
 {
-    @NotEmpty
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email is not valid")
     private String email;
-    @NotEmpty
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
